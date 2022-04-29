9a04caf345f720058bac9af94b729cde411c5db0
zihao_il <zihao1220@gmail.com>
2022-04-29 11:01:55 +0800

    Update mcskin.py

--- modules/mcskin.py
+++ modules/mcskin.py
@@ -5,7 +5,7 @@ import requests
 from graia.ariadne.app import Ariadne
 from graia.ariadne.event.message import GroupMessage
 from graia.ariadne.message.chain import MessageChain
-from graia.ariadne.message.element import Image, Plain
+from graia.ariadne.message.element import Image, Plain, At
 from graia.ariadne.message.parser.base import MatchContent
 from graia.ariadne.message.parser.twilight import FullMatch, Twilight, SpacePolicy, WildcardMatch, MatchResult
 from graia.ariadne.model import Group, Member
@@ -17,25 +17,36 @@ saya = Saya.current()
 channel = Channel.current()
 inc = InterruptControl(saya.broadcast)
 
-
-def getuuid(mcid):
-    uuid = requests.get('https://api.mojang.com/users/profiles/minecraft/' + mcid).text
-    uuid = json.loads(uuid)['id']
-    return uuid
-
-def getskin(uuid):
-        skinj = requests.get('https://sessionserver.mojang.com/session/minecraft/profile/'+uuid).text
-        skinj = json.loads(skinj)
-        b64 = skinj['properties'][0]['value']
-        scurl = base64.b64decode(b64).decode("utf-8", "ignore")
-        scurl = json.loads(scurl)
-        skinurl = scurl['textures']['SKIN']['url']
-        try:
-            capeurl = scurl['textures']['CAPE']['url']
-        except:
-            return skinurl
-        return skinurl,capeurl
-
+def getskin(mcid):
+    try:
+        uuid = requests.get('https://api.mojang.com/users/profiles/minecraft/' + mcid).text
+        uuid = json.loads(uuid)['id']
+    except:
+        msg = [
+            Plain("获取失败！用户不存在"),
+        ]
+        return msg
+    skinj = requests.get('https://sessionserver.mojang.com/session/minecraft/profile/'+uuid).text
+    skinj = json.loads(skinj)
+    b64 = skinj['properties'][0]['value']
+    scurl = base64.b64decode(b64).decode("utf-8", "ignore")
+    scurl = json.loads(scurl)
+    skinurl = scurl['textures']['SKIN']['url']
+    try:
+        capeurl = scurl['textures']['CAPE']['url']
+    except:
+        msg = [
+            Plain("名字: " + mcid.asDisplay() + "\n皮肤："),
+            Image(url=skinurl),
+        ]
+        return msg
+    msg = [
+        Plain("名字: " + mcid.asDisplay() + "\n皮肤："),
+        Image(url=skinurl),
+        Plain("\n披风："),
+        Image(url=capeurl),
+    ]
+    return msg
 
 @channel.use(ListenerSchema(
     listening_events=[GroupMessage],
@@ -46,32 +57,7 @@ def getskin(uuid):
 ))
 async def mcname(app: Ariadne, group: Group, member: Member,mcid: MatchResult):
     mcid = mcid.result.replace(" ","")
-    try:
-        uuid = getuuid(str(mcid))
-    except:
-        await app.sendMessage(group, MessageChain.create("查无此人"))
-        return
-    skinurl = getskin(uuid)
-
-    if len(skinurl) == 2:
-        await app.sendMessage(
-            group,
-            MessageChain.create(
-                Plain("名字: " + mcid.asDisplay() + "\n皮肤："),
-                Image(url=skinurl[0]),
-                Plain("\n披风："),
-                Image(url=skinurl[1]),
-            ),
-        )
-    else:
-        await app.sendMessage(
-            group,
-            MessageChain.create(
-                Plain("名字: " + mcid.asDisplay() + "\n皮肤："),
-                Image(url=skinurl),
-            ),
-        )
-
+    await app.sendMessage(group, MessageChain.create(getskin(mcid)))
 
 @channel.use(ListenerSchema(listening_events=[GroupMessage], decorators=[MatchContent("皮肤获取")]))
 async def mcname(app: Ariadne, group: Group, member: Member):
@@ -86,28 +72,4 @@ async def mcname(app: Ariadne, group: Group, member: Member):
     except asyncio.TimeoutError:
         await app.sendMessage(group, MessageChain.create("超时了，请重新发送"))
     else:
-        try:
-            uuid = getuuid(str(mcid))
-        except:
-            await app.sendMessage(group, MessageChain.create("查无此人"))
-            return
-        skinurl = getskin(uuid)
-
-        if len(skinurl) == 2:
-            await app.sendMessage(
-                group,
-                MessageChain.create(
-                    Plain("名字: " + mcid.asDisplay() + "\n皮肤："),
-                    Image(url=skinurl[0]),
-                    Plain("\n披风："),
-                    Image(url=skinurl[1]),
-                ),
-            )
-        else:
-            await app.sendMessage(
-                group,
-                MessageChain.create(
-                    Plain("名字: " + mcid.asDisplay() + "\n皮肤："),
-                    Image(url=skinurl),
-                ),
-            )
+        await app.sendMessage(group, MessageChain.create(getskin(mcid)))
